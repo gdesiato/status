cfd14a4fbc50fb60a07fed5703caa5c499663ff3
Giuseppe Desiato <giodesiato@gmail.com>
2022-09-23 15:31:00 -0400

    first commit

---
+++ src/main/java/com/giuseppe/status/StatusProjectApplication.java
@@ -0,0 +1,13 @@
+package com.giuseppe.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giuseppe/status/StatusProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuseppe.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
